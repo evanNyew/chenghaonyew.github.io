ffb7ae456f6363036012cebd42126917560ea6ef
Cheng Hao Nyew <nyewch0404@gmail.com>
2021-07-08 01:36:20 +0800

    React Tesla Clone

---
+++ React Clone  Project/Tesla Clone Youtube/tesla-clone-youtube/src/components/Section.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import styled from "styled-components"
+import Fade from 'react-reveal/Fade'
+
+function Section({title, description, backgroundImg, leftBtnText, rightBtnText}) {
+    return (
+        <Wrap bgImage={backgroundImg}>
+            {/* Fade animation from react reveal. Animation from bottom to top */}
+            <Fade bottom>
+                <ItemText>
+                    <h1>{title}</h1>
+                    <p>{description}</p>
+                </ItemText>
+            </Fade>
+            <Buttons>
+                {/* Fade animation from react reveal. Animation from bottom to top */}
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>
+                            {leftBtnText}
+                        </LeftButton>
+                        {/* If rightBtnText exists, then show the RightButton component */}
+                        { rightBtnText &&  
+                            <RightButton>
+                                {rightBtnText}
+                            </RightButton>
+                        }
+                    </ButtonGroup>
+                    <DownArrow src="/images/down-arrow.svg"/>
+                </Fade>
+            </Buttons>
+        </Wrap>
+    )
+}
+
+export default Section
+
+const Wrap = styled.div`
+    width: 100vw; 
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: ${props => `url("images/${props.bgImage}")`};
+
+    // To seperate Text Component & Button Group
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;     // vertical alignment
+    align-items: center;                 // horizontal 
+`
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+
+    // it applied the following css rules until max width of 768px 
+    @media (max-width: 768px){
+        flex-direction: column;
+    }
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    margin: 8px;
+`
+
+const RightButton = styled(LeftButton)`
+    background: white;
+    opacity: 0.65;
+    color: black;
+`
+
+const DownArrow = styled.img`
+    height: 40px;
+    overflow-x: hidden;
+    animation: animateDown infinite 1.5s;
+`
+
+const Buttons = styled.div`
+
+`
